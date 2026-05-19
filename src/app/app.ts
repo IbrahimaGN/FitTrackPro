b0ae42ea31cff2c186f92163df813c2e47f60748
IbrahimaGN <gningkhalilo49@gmail.com>
2026-05-19 18:42:35 +0000

    V0

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,6 @@ import { HeaderComponent } from './layout/header/header';
 import { SidebarComponent } from './layout/sidebar/sidebar';
 
 @Component({
-  
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, SidebarComponent],
